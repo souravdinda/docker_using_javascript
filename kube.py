b456db69cc9fcef5cdf3cd13ce4a1a379e98306e
souravdinda <souravdinda14@gmail.com>
2021-06-30 07:46:22 +0000

    s

---
+++ kube.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import cgi
+import subprocess
+
+print("context-type: text/html")
+print()
+
+
+f = cgi.FieldStorage()
+cmd = f.getvalue("x")
+#cmd= "kubectl get pods"
+a = "sudo "  +  cmd + "   --kubeconfig admin.conf" 
+#print(a)
+output = subprocess.getoutput(a)
+print(output)
